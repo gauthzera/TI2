2400f8c47213c4ba89528fd09998ea69c997d45b
Gabriel Ulhoa Thebaldi <gabrielulhoa@outlook.com>
2026-03-01 10:12:15 -0300

    exercício 1 ti2

---
+++ src/main/java/exercicio1/Main.java
@@ -0,0 +1,17 @@
+package exercicio1;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		
+		int soma=x+y;
+		System.out.println(soma);
+		
+		sc.close();
+	}
+}
