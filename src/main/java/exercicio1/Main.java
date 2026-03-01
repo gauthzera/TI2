9885cdfb34862f22ce2a6d719a2f033cd623e902
Gabriel Ulhoa Thebaldi <gabrielulhoa@outlook.com>
2026-03-01 10:24:52 -0300

    ex1

--- src/main/java/exercicio1/Main.java
+++ src/main/java/exercicio1/Main.java
@@ -6,10 +6,13 @@ public class Main {
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
 		
+		System.out.print("Digite um número: ");
 		int x=sc.nextInt();
+		System.out.print("Digite um número: ");
 		int y=sc.nextInt();
 		
 		int soma=x+y;
+		System.out.print("Soma: ");
 		System.out.println(soma);
 		
 		sc.close();
